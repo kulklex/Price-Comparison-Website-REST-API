d4c664334b79787614843f6a1b2a93d696ab06c4
kulklex <kulklex@gmail.com>
2023-12-12 23:37:43 +0000

    Test for search utility

---
+++ utils.js
@@ -0,0 +1,25 @@
+const db = require("./database/db")
+
+
+/** Function that gets the total count of product from search term */
+async function getSearchCount(searchTerm) {
+    const sql = `SELECT COUNT(*) FROM Product WHERE name LIKE '%${searchTerm}%'`;
+    let result = await db.query(sql);
+    return result[0]["COUNT(*)"];
+  }
+  
+  /** Function that searches for product with search term */
+ async function search(searchTerm, numItems, offset) {
+    let sql = `SELECT * FROM Product WHERE name LIKE '%${searchTerm}%' `;
+  
+    //Limit the number of results returned
+    if (numItems !== undefined && offset !== undefined) {
+      sql += `LIMIT ${numItems} OFFSET ${offset}`;
+    }
+  
+    return db.query(sql);
+  }
+
+
+
+module.exports = {getSearchCount, search}
